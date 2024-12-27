d169a6244d6e32a60fd9316208e282718d33c027
farea <faraa717281413@gmail.com>
2024-12-27 16:38:51 +0300

    this is simple Example Internet Connect checker

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+<<<<<<< HEAD
 import flutter_local_notifications
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+=======
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+>>>>>>> eea456b (this is simple Example Internet Connection Checker)
 }
